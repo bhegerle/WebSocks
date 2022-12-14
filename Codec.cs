0a9260d6c39ff27522d643c000f2166671e531e7
Blake Hegerle <blake.hegerle@allianzlife.com>
2022-12-14 09:51:08 -0600

    added codec class

--- Codec.cs
+++ Codec.cs
@@ -6,7 +6,11 @@ namespace shock;
 internal class Codec
 {
     private const int KEY_LEN = 16;
+    private static readonly int NONCE_LEN = AesCcm.NonceByteSizes.MinSize;
+    private static readonly int TAG_LEN = AesCcm.TagByteSizes.MinSize;
+
     private readonly AesCcm Cipher;
+    private readonly RandomNumberGenerator Rng;
 
     internal Codec(string key)
     {
@@ -14,7 +18,44 @@ internal class Codec
         var dkey = HKDF.DeriveKey(HashAlgorithmName.SHA256, ikm, KEY_LEN);
         Cipher = new AesCcm(dkey);
 
-        var nonce = new byte[10];
-        RandomNumberGenerator.Create().GetBytes(nonce);
+        Rng = RandomNumberGenerator.Create();
+    }
+
+    internal void Encode(byte[] data, Stream stream)
+    {
+        var nonce = new byte[NONCE_LEN];
+        Rng.GetBytes(nonce);
+
+        var tag = new byte[TAG_LEN];
+
+        var ciphertext = new byte[data.Length];
+        Cipher.Encrypt(nonce, data, ciphertext, tag);
+
+        stream.Write(nonce);
+        stream.Write(tag);
+        stream.Write(BitConverter.GetBytes(ciphertext.Length));
+        stream.Write(ciphertext);
+    }
+
+    internal byte[] Decode(Stream stream)
+    {
+        var nonce = Read(stream, NONCE_LEN);
+        var tag = Read(stream, TAG_LEN);
+        var len = Read(stream, sizeof(int));
+        var ciphertext = Read(stream, BitConverter.ToInt32(len));
+
+        var plaintext = new byte[ciphertext.Length];
+        Cipher.Decrypt(nonce, ciphertext, tag, plaintext);
+
+        return plaintext;
+    }
+
+    private static byte[] Read(Stream stream, int n)
+    {
+        var buffer = new byte[n];
+        var r = stream.Read(buffer);
+        if (r != n)
+            throw new Exception("incomplete read");
+        return buffer;
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,5 +4,15 @@ if (args.Length == 0)
     throw new Exception("usage: shock.exe {config path}");
 
 var config = await Config.Load(args[0]);
-new Codec(config.Key);
+
+var buffer = new byte[3];
+var stream = new MemoryStream();
+var c = new Codec(config.Key);
+c.Encode(buffer, stream);
+
+stream.Seek(0, SeekOrigin.Begin);
+
+var p = c.Decode(stream);
+
+
 Console.WriteLine();
