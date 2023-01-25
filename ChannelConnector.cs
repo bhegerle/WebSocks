535281d2fc1f85f74be0dc7de69ccf4517fe0190
Blake Hegerle <blake.hegerle@allianzlife.com>
2023-01-25 17:54:00 -0600

    keep track of reconnect attempts

--- ChannelConnector.cs
+++ ChannelConnector.cs
@@ -12,6 +12,7 @@ internal class ChannelConnector {
     private readonly ProtocolByte protoByte;
     private readonly Config config;
     private readonly IWebSocketSource wsSrc;
+    private int attempt;
 
     internal ChannelConnector(ProtocolByte protoByte, Config config, IWebSocketSource wsSrc) {
         this.protoByte = protoByte;
@@ -19,8 +20,9 @@ internal class ChannelConnector {
         this.wsSrc = wsSrc;
     }
 
-    internal async Task<Channel> Connect(CancellationToken token, bool isReconnect) {
-        if (isReconnect)
+    internal async Task<Channel> Connect(CancellationToken token) {
+        attempt++;
+        if (attempt > 1)
             await Task.Delay(config.ReconnectDelay, token);
 
         WebSocket ws = null;
@@ -36,7 +38,7 @@ internal class ChannelConnector {
             await c.HandshakeCheck(token);
             return c;
         } catch (Exception e) {
-            ws.Dispose();
+            ws?.Dispose();
             c?.Dispose();
             throw new ChannelConnectionException(e);
         }
